3192cf8bf280fb44875e5dd25e286c94edb9fe6d
nityag20 <61954408+nityagop@users.noreply.github.com>
2021-07-25 03:33:54 +0000

    Initial commit

---
+++ quiz.js
@@ -0,0 +1,178 @@
+var scientists = [
+   {
+    name: "Cynthia Kenyon",
+    intro: "a molecular biologist, researching ways to lengthen human lives. Her work in STEM is leading the way to a brighter future. ",
+    img: "https://researcherprofiles.org/profile/Modules/CustomViewPersonGeneralInfo/PhotoHandler.ashx?NodeID=182239"
+  },
+
+ {
+    name: "Kizzmekia Corbett",
+    intro: "a viral immunologist, helped research and create the Moderna vaccine. She ius a Shutzer Assistant Professor at the Harcard Radcliffe Unstitute and professor of immunology and infectious diseases at Harvard", img: "https://www.kapitalafrik.com/wp-content/uploads/2021/05/Kizzmekia-Kizzy-Shanta-Corbett-copie.jpg"
+  },
+
+{
+    name: "Tu Youyou",
+    intro: "a pharmaceutical chemist, discovered a treatment for malaria, won the 2015 Nobel Prize in medicine. She discovered artemisinin and dihydroartemisinin, used to treat malaria, a breakthrough in twentieth-century tropical medicine, saving millions of lives in South China, Southeast Asia, Africa, and South America",
+    img: "https://static01.nyt.com/images/2015/10/06/science/07tuyouyou-web1/07tuyouyou-web1-superJumbo.jpg"
+  },
+
+  {
+    name: "Shobhana Narasimhan",
+    intro: "a professor of Theoretical Sciences, researches computational nanoscience. At age 57, she works at the Hawaharlal Nehru center for advanced scientific research. ", 
+    img: "https://www.jncasr.ac.in/sites/default/files/Faculty-Profile/2021-02/Shobhana.jpg"
+  },
+
+ {
+    name: "Jennifer Doudna",
+    intro: "a biochemist, developed CRISPR-Cas9, won the 2020 Nobel Peace Prize in Chemistry ith Emmanuelle Charpentier, for the development of a method for genome editing.",
+    img: "https://www.browndailyherald.com/wp-content/uploads/2021/03/JenniferDoudna.jpg"
+  },
+];
+ var techies = [
+  {
+    name: "Reshma Saujani",
+    intro: "an extroardinary leader in diversifying STEM, the founder/CEO of girls who code, works to close the gender gap in technology. She also works as a lawyer and graduated from Harvard University in 1999",
+    img: "https://wsbaws-websitefs-prod.s3.amazonaws.com/2019/06/29/17/45/08/00bfbdb1-fc83-4943-bc31-c1aece58bc71/Saujani_Reshma_PROMOPICL_CreditSalonMediaGroup%2090.jpg"
+  },
+  {
+    name: "Susan Wojcicki",
+    intro: "the CEO of youtube, applied google's search engine, Influenced the adaptation of VR/3D. She has been in the tech industry for over 20 years. Wojcicki was involved in the founding of Google, and became Google's first marketing manager in 1999.",
+    img: "https://pbs.twimg.com/profile_images/1316183322192568330/QFz_ARWk_400x400.jpg"
+  },
+  {
+    name: "Ellen K. Pao",
+    intro: "the CoFounder and CEO of Project Include, Former CEO of reddit. She also works as an investor. Pao first became known in 2012 for filing an unsuccessful gender discrimination suit against her employer",
+    img: "https://media.wired.com/photos/59c1389e0dc34b671d10ebf3/master/w_2560%2Cc_limit/GettyImages-527764824.jpg"
+  },
+  {
+    name: "Danah Boyd",
+    intro: "the Founder/presidents of Data&society, Partner Researcher at Microsoft Research, Visiting Professor at NYU",
+    img: "https://upload.wikimedia.org/wikipedia/commons/f/f6/Dboyd-3.jpg"
+  },
+  {
+    name: "Kimberlay Bryant",
+    intro: "the Founder/CEO of Black girls who code, opportunities for women/girls in the tech. Her work is very crucial for the future of STEM. By 2040, Black Girls Code will have trained 1 million bright young minds.", img: "https://s.hdnux.com/photos/57/65/66/12541325/4/rawImage.jpg"
+  }
+]
+//💻💻💻💻💻💻
+var engineers = [
+  {
+    name: "Christina Koch",
+    intro: "an Electrical engineer at NASA's Goddard Space Flight Center. She is also a NASA austonautt of the class of 2013.",
+    img: "https://images.firstpost.com/wp-content/uploads/2019/04/Astronaut-Christina-Koch_NASA-4.jpg"
+  },
+  {
+    name: "Kate Weimer",
+    intro: "revolutionizing healthcare by combining powerful digital workflows with the ability to create templates, guides, instruments, and even implants using 3D-printing technologies",
+    img: "https://i0.wp.com/womenin3dprinting.com/wp-content/uploads/2018/09/Katie-Weimer-Headshot-08_2018-e1593829227311.jpg?fit=600%2C600&ssl=1"
+  },
+  {
+    name: "Gwynne Shotwell",
+    intro: "the President and COO of SpaceX. Under her technical guidance, SpaceX has completed many successful missions and plans to send astronauts to ISS and then to Mars in the 2020s.",
+    img: "https://s3.amazonaws.com/marquee-test-akiaisur2rgicbmpehea/2c2bI9iaQ0OaYF4dW2la_Shotwell%202017.jpg"
+  },
+  {
+    name: "Evelyn Wang",
+    intro: "the Head of the Department of Mechanical Engineering at MIT. She is transforming the engineering curriculum by implementing new educational models and grand research challenges that will impact the way mechanical engineering is being taught.",
+    img: "https://news.mit.edu/sites/default/files/images/201806/evelyn-wang-mit-meche-A1.jpg"
+  },
+  {
+    name: "Ramille Shah",
+    intro: "the Chief Scientific Officer, advancing the biomaterials & tissue engineering field with her expertise in developing and characterizing new biomaterial strategies for tissue and organ engineering.",
+    img: "http://www.awis-chicago.org/wp-content/uploads/2011/10/ramille-lab-pic1-290x300.jpg"
+  }
+]
+
+//🔎🔎🔎🔎🔎🔎
+var mathematicians = [
+  {
+    name: "Dr. Karen Uhlenbeck",
+    intro: "known for her discoveries & contributions in geometrical analysis, quantam field theory, etc.",
+    img: "https://static01.nyt.com/images/2019/03/19/science/19ABEL1/19ABEL1-jumbo.jpg"
+  },
+  {
+    name: "Grace Alele Williams",
+    intro: "First Nigerian Vice-Chancellor at the university of Benin and first Nigerian woman to receive a doctorate degree. As vice chancellor she made useful contributions in the development of education in Nigeria. She is a Merit award winner and Regional Vice President for Africa of the Third World Organization for Women in Science", 
+    img: "https://saharatimesgroup.com/wp-content/uploads/2021/02/98CA52CA-03A8-4DED-AFF6-5AFB71149E12.jpeg"
+  },
+  {
+    name: "Evelyn Boyd Granville",
+    intro: "Received a PHD from Yale University in 1945, and was a pioneer in math and computer science. Early NASA member. She was a powerful voice for women in stem.",
+    img: "https://miro.medium.com/max/376/1*0-oYmmemOzdntOG8bcPOuQ.jpeg"
+    // img: "https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2016/01/evelyn_granville.jpg"
+  },
+  {
+    name: "Katherine Johnson",
+    intro: "Awarded the presidential medal of freedom(2015). Brilliant mathematician who’s calculations of orbital mechanics was critical in the launch of the first space flight. Nicknamed NASA;s human computer",
+    img: "https://media.npr.org/assets/img/2017/03/06/ap_17058111892412_vert-3a224ee05bf259a676680d128dacbacd8ef261ac.jpg"
+  },
+  {
+    name: "Valerie Thomas",
+    intro: "Best known for inventing the Illusion Transmitter - a 3D imaging technology that was the first of its kind when it debuted in 1980. She worked as a developer and engineer at NASA for 30 years.",
+    img: "https://www.galaxycity.us/wp-content/uploads/2020/01/valerie-thomas.jpg"
+  }
+]
+
+//todo
+//intro so that every intro works with a sentance✅
+//change all photos fro, links to image addresses!!✅
+//nav bar✅
+//CSS ✅
+//make it delete and print a new one✅
+
+// If science is chosen🔬🔬🔬🔬🔬
+var scienceButton = $('.scienceButton');
+scienceButton.one("click", scienceInterest);
+
+function scienceInterest(event) {
+  var random = Math.round(Math.random() * 4);
+
+  var fill = $(".fill");
+  var scientist = scientists[random];
+  fill.append(`<br> <p class = "text">🔬🔬🔬🔬🔬<br> You should learn about <b>${scientist.name}</b>! She is ${scientist.intro}. </p> <br> <img class = "images" src = ${scientist.img}>`);
+
+  event.preventDefault();
+
+}
+
+//if tech is chosen👩‍💻👩‍💻👩‍💻👩‍💻👩‍💻👩‍💻👩‍💻
+var techButton = $('.technology');
+techButton.one("click", techIntrest);
+
+function techIntrest(event) {
+var random = Math.round(Math.random() * 4);
+
+var fill = $(".fill");
+var techy = techies[random];
+fill.append(`<br> <p class = "text">👩‍💻👩‍💻👩‍💻👩‍💻👩‍💻👩‍💻👩‍💻<br>You should learn about <b>${techy.name}</b>! She is ${techy.intro}. </p> <br> <img class = "images" src = ${techy.img}><br>`);
+
+event.preventDefault();
+}
+
+//if engineering is chosen
+var codeButton = $('.engineering');
+codeButton.one("click", codeIntrest);
+
+function codeIntrest(event) {
+var random = Math.round(Math.random() * 4);
+
+var fill = $(".fill");
+var cs = engineers[random];
+fill.append(`<br> <p class = "text">💻💻💻💻💻💻<br>You should learn about <b>${cs.name}</b>! She is ${cs.intro}. </p> <br> <img class = "images" src = ${cs.img}><br>`);
+
+event.preventDefault();
+}
+
+//if math is chosen
+var mathButton = $('.math');
+mathButton.one("click", mathIntrest);
+
+function mathIntrest(event) {
+var random = Math.round(Math.random() * 4);
+
+var fill = $(".fill");
+var mathers = mathematicians[random];
+fill.append(`<br> <p class = "text">📘📘📘📘📘📘<br>You should learn about <b>${mathers.name}</b>! She is ${mathers.intro}. </p> <br> <img class = "images" src = ${mathers.img}><br>`);
+
+event.preventDefault();
+}
